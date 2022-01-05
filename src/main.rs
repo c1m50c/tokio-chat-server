8949c686a3a9ab27e0d797b12cc70d6c37404921
c1m50c <terminalityms@gmail.com>
2022-01-05 11:23:16 -0600

    Echo Server `Hello, World!` 🔊🌎

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
-fn main() {
-    println!("Hello, world!");
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::net::TcpListener;
+use std::error::Error;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let listener = TcpListener::bind("localhost:8080").await?;
+    
+    loop {
+        let (mut socket, _addr) = listener.accept().await?;
+
+        tokio::spawn(async move {
+            let mut buffer = [0; 1024];
+
+            loop {
+                let n = match socket.read(&mut buffer).await {
+                    Ok(n) if n == 0 => return,
+                    Ok(n) => n,
+
+                    Err(e) => {
+                        eprintln!("Failed to read from socket, Error = {:?}.", e);
+                        return;
+                    },
+                };
+
+                if let Err(e) = socket.write_all(&buffer[0 .. n]).await {
+                    eprintln!("Failed to write to socket, Error = {:?}.", e);
+                    return;
+                }
+            }
+        });
+    }
 }
